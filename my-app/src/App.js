3ad03822d9cef7f637145153a6ff6a36e6886759
Dorene07 <dstmarth@gmail.com>
2024-03-02 18:02:59 -0500

    navbar initializing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div >
-    <h1 className= "text-blue-600" >Shape My Weigh</h1>
+    <h1 className= 'text-yellow'>Shape My Weigh</h1>
      
     </div>
   );
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -4,8 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'black': '#000000',
+      'midnight': '#14213d',
+      'yellow': '#fca311',
+      'gray': '#e5e5e5',
+      'white': '#ffffff',
     extend: {},
   },
+},
   plugins: [],
 }
 
