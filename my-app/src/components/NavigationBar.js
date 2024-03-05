c25a74e61396c9849c068a6e292c3e00efc52c1a
Dorene07 <dstmarth@gmail.com>
2024-03-04 19:11:34 -0500

    naviagation bar info

--- my-app/src/components/NavigationBar.js
+++ my-app/src/components/NavigationBar.js
@@ -6,6 +6,18 @@ const NavigationBar = () => {
       <div className="flex items-center">
         <img src="./logo.png" alt="Logo" className="h-16 mr-" />
       </div>
+      <div className="flex justify-center items-center">
+        <a href="#" className="text-2xl relative hover:text-marigold belanosima-regular mr-8">
+          About
+        </a>
+        <a href="#" className="text-2xl relative hover:text-marigold belanosima-regular mr-8">
+          Review
+        </a>
+        <a href="#" className="text-2xl relative hover:text-marigold belanosima-regular mr-8">
+          Contact Us
+        </a>
+      </div>
+
       <div>
         <a href="#" className="text-2xl relative hover:text-marigold belanosima-regular mr-4">
           <button className="bg-white text-black px-6 py-3 rounded-md shadow-md hover:shadow-lg">Login</button>  
